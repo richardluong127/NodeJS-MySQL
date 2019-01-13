501637e90af59e74edaa9a577893e91493db3184
richardluong127 <richardluong127@gmail.com>
2019-01-13 10:35:04 -0800

    updated for submission

---
+++ README.md
@@ -0,0 +1,16 @@
+Bamazon!
+
+Make purchases using Bamazon node.  Utilizes MySql database and inquirer npm.
+
+When the application starts, you'll be shown a database of items available for purchase and their associated prices, department, and quantity on hand.
+
+1. Type in the name of the item (case sensitive) you would like to purchase.
+2. Enter the numerical quantity of the item.
+3. If the numerical quantity exceeds quantity on hand, you will be notified.
+
+Happy shopping!
+
+Here's an example of what to expect:
+
+![Bamazon App](liri-node-app/LiriExample.png)
+Format: ![Alt Text](liri-node-app/LiriExample.png)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -11,22 +11,21 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
+    console.log("Connection successful.");
     console.log("Welcome to Bamazon!");
-    start();
+    showProducts();
 });
 
-var start = function () {
+var showProducts = function () {
     connection.query("SELECT * FROM products", function (err, res) {
         for (var i = 0; i < res.length; i++) {
-            console.log(res[i].item_id + " || " + res[i].product_name + " || Dept: " + res[i].department_name + " || Price: " + res[i].price + " || Qty Avail: " + res[i].stock_quantity + "\n");
+            console.log(res[i].item_id + " || " + res[i].product_name + " || Dept: " + res[i].department_name + " || Price: " + res[i].price + " || Qty Avail: " + res[i].stock_quantity);
         }
-        console.log("This is response 1: ",res);
         askCustomer(res);
     })
 }
 
 var askCustomer = function (res) {
-    console.log("This is response 2: ", res);
     inquirer
         .prompt({
             type: "input",
@@ -34,44 +33,57 @@ var askCustomer = function (res) {
             message: "What would you like to purchase?",
 
         }).then(function (answer) {
-
+            var correct = false;
             for (var i = 0; i < res.length; i++) {
+
                 if (res[i].product_name === answer.choice) {
+                    correct = true;
                     var id = i;
                     inquirer.prompt([{
                         type: "input",
                         name: "quantity",
                         message: "How many would you like to buy?",
                         validate: function (value) {
-                            if (isNaN(value) === false) {
+                            if (isNaN(value) == false) {
                                 return true;
                             }
                             return false;
                         }
                     }]).then(function (answer) {
                         var newQty = res[id].stock_quantity - answer.quantity;
-                        if (newQty > 0) {
+
+                        if (newQty >= 0) {
+
                             connection.query("UPDATE products SET ? WHERE ?",
                                 [{
-                                    stock_quantity: newQty
+                                    stock_quantity: res[id].stock_quantity - answer.quantity
                                 },
                                 {
                                     product_name: answer.choice
-                                }]
-                                ,
-                                function (err) {
+                                }],
+
+                                function (err, res2) {
                                     if (err) throw err;
                                     console.log("Item successfully purchased!");
-                                    askCustomer(res);
+                                    showProducts();
                                 }
                             )
-                        } else {
+                        }
+                        else {
                             console.log("Not enough quantity on hand!");
                             askCustomer(res);
-                    }}
+                        }
+                    }
                     )
+                }
+                if (i == res.length && correct == false) {
+                    console.log("Not a valid selection!");
+                    askCustomer(res);
                 }
             }
         })
 }
-
+//Make new table balance which will udpate as user makes purchases.
+// var currentBalance = 
+// var totalPrice = res[i].price * answer.quantity + currentBalance;
+// console.log("Your total for this order is currently: $"+totalPrice); 
